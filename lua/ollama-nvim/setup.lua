afef0bd87e62e607868267434f1f4db180b156cd
Jayden <jpylesbusiness@gmail.com>
2024-02-08 21:08:27 -0600

    fix: pathing

--- lua/ollama-nvim/setup.lua
+++ lua/ollama-nvim/setup.lua
@@ -1,11 +1,9 @@
 local M = {}
 
 local function setup_virtual_env_internal()
-	-- Dynamically determine the root path for the project installation
 	local project_root = vim.fn.stdpath("data") .. "/lazy/ollama-nvim"
 	local venv_path = project_root .. "/venv"
 	local requirements_path = project_root .. "/requirements.txt"
-	print(venv_path)
 
 	-- Check if the virtual environment already exists
 	if vim.fn.isdirectory(venv_path) == 0 then
@@ -16,10 +14,12 @@ local function setup_virtual_env_internal()
 		print("Virtual environment already exists.")
 	end
 
-	-- Activate the virtual environment and install requirements
-	-- Note: This command may vary depending on your operating system and shell
-	local activate_command = "bash source " .. venv_path .. "/bin/activate"
-	local pip_install_command = activate_command .. " && pip install -r " .. requirements_path
+	-- Correct way to activate the virtual environment and install requirements
+	local pip_install_command = "bash -c 'source "
+		.. venv_path
+		.. "/bin/activate && pip install -r "
+		.. requirements_path
+		.. "'"
 	local result = vim.fn.system(pip_install_command)
 
 	if vim.v.shell_error == 0 then
