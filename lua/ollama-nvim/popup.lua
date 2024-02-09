531de79d889ee27543f4d6b8e2b3000cde0076f9
Jayden <jpylesbusiness@gmail.com>
2024-02-09 10:31:36 -0600

    wip: test layout

--- lua/ollama-nvim/popup.lua
+++ lua/ollama-nvim/popup.lua
@@ -47,6 +47,34 @@ local function show_my_popup()
 	end, { noremap = true })
 end
 
+local Layout = require("nui.layout")
+local Popup = require("nui.popup")
+
+local function test_layout()
+	local top_popup = Popup({ border = "double" })
+	local bottom_left_popup = Popup({ border = "single" })
+	local bottom_right_popup = Popup({ border = "single" })
+
+	local layout = Layout(
+		{
+			position = "50%",
+			size = {
+				width = 80,
+				height = 40,
+			},
+		},
+		Layout.Box({
+			Layout.Box(top_popup, { size = "40%" }),
+			Layout.Box({
+				Layout.Box(bottom_left_popup, { size = "50%" }),
+				Layout.Box(bottom_right_popup, { size = "50%" }),
+			}, { dir = "row", size = "60%" }),
+		}, { dir = "col" })
+	)
+
+	layout:mount()
+end
+
 return {
-	show_my_popup = show_my_popup,
+	show_my_popup = test_layout(),
 }
