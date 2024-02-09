d5227a9c7ddfe34eecdfc7a32d8a19199ce38e21
Jayden <jpylesbusiness@gmail.com>
2024-02-09 09:35:30 -0600

    wip: test popup

--- lua/ollama-nvim/init.lua
+++ lua/ollama-nvim/init.lua
@@ -1,5 +1,6 @@
 local core = require("ollama-nvim.core") -- Correctly requiring the core module
 local setup = require("ollama-nvim.setup")
+local popup = require("ollama-nvim.popup")
 
 local M = {}
 
@@ -13,6 +14,7 @@ end
 
 vim.api.nvim_create_user_command("QueryLLM", M.queryLLM, { nargs = 0 })
 vim.api.nvim_create_user_command("QueryLLMSetup", setup.setup_virtual_env, { nargs = 0 })
+vim.api.nvim_create_user_command("TestPopup", popup.show_my_popup, { nargs = 0 })
 
 vim.api.nvim_set_keymap(
 	"v",
---
+++ lua/ollama-nvim/popup.lua
@@ -0,0 +1,35 @@
+local Popup = require("nui.popup")
+local event = require("nui.utils.autocmd").event
+
+local function show_my_popup()
+	local my_popup = Popup({
+		border = {
+			style = "rounded", -- or "single", "double", "shadow", etc.
+			text = {
+				top = "Popup Title",
+				top_align = "center",
+			},
+		},
+		position = "50%",
+		size = {
+			width = 40,
+			height = 10,
+		},
+		enter = true,
+		focusable = true,
+		zindex = 50, -- ensures popup is above other UI components
+	})
+
+	-- Set content of the popup window
+	my_popup:mount()
+	my_popup:set_lines({ "Line 1", "Line 2", "Line 3" }) -- Example content
+
+	-- Optional: Add keymaps for closing the popup
+	my_popup:map("n", "<Esc>", function()
+		my_popup:unmount()
+	end, { noremap = true })
+end
+
+return {
+	show_my_popup = show_my_popup,
+}
