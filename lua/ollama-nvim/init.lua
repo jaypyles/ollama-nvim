5db112d90bf344686f7a7cc03342c51d2959d17a
Jayden <jpylesbusiness@gmail.com>
2024-02-09 15:55:26 -0600

    func

--- lua/ollama-nvim/init.lua
+++ lua/ollama-nvim/init.lua
@@ -14,7 +14,7 @@ end
 
 vim.api.nvim_create_user_command("QueryLLM", M.queryLLM, { nargs = 0 })
 vim.api.nvim_create_user_command("QueryLLMSetup", setup.setup_virtual_env, { nargs = 0 })
-vim.api.nvim_create_user_command("TestPopup", popup.show_my_popup, { nargs = 0 })
+vim.api.nvim_create_user_command("TestPopup", popup.make_prompt, { nargs = 0 })
 
 vim.api.nvim_set_keymap(
 	"v",
