d018516ebd94558e0fbda13fed6d3838806ef153
Jayden <jpylesbusiness@gmail.com>
2024-02-09 09:54:14 -0600

    wip: create a scratch buffer

--- lua/ollama-nvim/popup.lua
+++ lua/ollama-nvim/popup.lua
@@ -3,42 +3,31 @@ local event = require("nui.utils.autocmd").event
 
 local function show_my_popup()
 	local my_popup = Popup({
+		border = {
+			style = "rounded", -- or "single", "double", "shadow", etc.
+			text = {
+				top = "Popup Title",
+				top_align = "center",
+			},
+		},
 		position = "50%",
 		size = {
-			width = 80,
-			height = 40,
-		},
-		enter = true,
-		focusable = true,
-		zindex = 50,
-		relative = "editor",
-		border = {
-			padding = {
-				top = 2,
-				bottom = 2,
-				left = 3,
-				right = 3,
-			},
-			style = "rounded",
-			text = {
-				top = " I am top title ",
-				top_align = "center",
-				bottom = "I am bottom title",
-				bottom_align = "left",
-			},
+			width = 40,
+			height = 10,
 		},
 		buf_options = {
 			modifiable = true,
 			readonly = false,
 		},
-		win_options = {
-			winblend = 10,
-			winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
-		},
+		enter = true,
+		focusable = true,
+		zindex = 50, -- ensures popup is above other UI components
+		bufnr = vim.api.nvim_create_buf(false, true),
 	})
 
 	-- Set content of the popup window
 	my_popup:mount()
+	my_popup:set_lines({ "Line 1", "Line 2", "Line 3" }) -- Example content
 
 	-- Optional: Add keymaps for closing the popup
 	my_popup:map("n", "<Esc>", function()
